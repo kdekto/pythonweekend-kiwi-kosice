a28d457f4feb32ad3be548e32cb95ae4523a4a3e
Michal Deco <mdeco@gohealth.com>
2020-08-22 18:04:26 +0200

    K4 - Add order and payment functionality

--- myeshop/website/urls.py
+++ myeshop/website/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('addItem', views.add_item, name='addItem'),
+    path('createOrder', views.create_order, name='createOrder'),
+    path('payment', views.payment, name='payment')
 ]
